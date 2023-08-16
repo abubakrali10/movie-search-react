245f429beeacd2902e16e5cb935ed6847f186e88
abubakrali10 <bakrali175@yahoo.com>
2023-08-16 18:59:27 +0300

    add submit search button

--- src/components/Header/SearchBar.jsx
+++ src/components/Header/SearchBar.jsx
@@ -1,12 +1,30 @@
 import styles from './SearchBar.module.css';
 import searchIcon from '../../assets/icons8-search.svg';
+import { useState } from 'react';
 
 function SearchBar() {
+	const [searchQuery, setSearchQuery] = useState('');
+
+	const handleSearch = (e) => {
+		setSearchQuery(e.target.value);
+	}
+
 	return (
 		<div className={styles.searchBar}>
 			<form>
-				<input type='text' placeholder='Search for movies...' />
-				<img src={searchIcon} alt='search icon' className={styles.searchIcon} />
+				<input
+					type='text'
+					placeholder='Search for movies...'
+					value={searchQuery}
+					onChange={handleSearch}
+				/>
+				<button type="submit" className={styles.searchButton}>
+					<img
+						src={searchIcon}
+						alt='search icon'
+						className={styles.searchIcon} 
+					/>
+				</button>
 			</form>
 		</div>
 	)
