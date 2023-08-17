f8dba214e6b50a6d991bafe86413febb162e878d
abubakrali10 <bakrali175@yahoo.com>
2023-08-17 17:43:03 +0300

    add google fonts

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
+import { VitePluginFonts } from 'vite-plugin-fonts'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    VitePluginFonts({
+      google: {
+        families: [
+          'Roboto:400,500',
+        ],
+      },
+    }),
+    react(),
+  ],
+});
