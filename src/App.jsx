5de35f5793932d2f041b1606fd35e49ee6b68ee5
abubakrali10 <bakrali175@yahoo.com>
2023-08-17 03:18:33 +0300

    add test movie

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import React from "react"
 import Header from "./components/Header/Header"
 import './App.css';
 import styles from './App.module.css';
+import MovieItem from "./components/MovieList/MovieItem";
+import MovieList from "./components/MovieList/MovieList";
 
 function App() {
 
   return (
     <div className={`${styles.appContainer}`}>
       <Header />
+      <MovieList />
     </div>
   )
 }
