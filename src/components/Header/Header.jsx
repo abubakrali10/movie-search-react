fe34bfdeee8047f261da657e643bd80876fb27c9
abubakrali10 <bakrali175@yahoo.com>
2023-08-17 20:16:56 +0300

    Move SearchBar outside Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,10 @@
 import Logo from './Logo';
 import styles from './Header.module.css';
-import SearchBar from './SearchBar';
 
 function Header() {
 	return (
 		<header className={styles.header}>
 			<Logo />
-			<SearchBar />
 		</header>
 	)
 }
