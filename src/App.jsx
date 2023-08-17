b274c35d24da609d016e20a62375b6ae76bafe78
abubakrali10 <bakrali175@yahoo.com>
2023-08-17 17:45:29 +0300

    App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,25 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import Header from "./components/Header/Header"
 import './App.css';
 import styles from './App.module.css';
-import MovieItem from "./components/MovieList/MovieItem";
 import MovieList from "./components/MovieList/MovieList";
+import { getPopularMovies } from "./services/api";
 
 function App() {
+  const [popularMovies, setPopularMovies] = useState([]);
+
+  useEffect(() => {
+    async function fetchPopularMovies() {
+      const movies = await getPopularMovies();
+      setPopularMovies(movies);
+    }
+    fetchPopularMovies();
+  }, []);
 
   return (
     <div className={`${styles.appContainer}`}>
       <Header />
-      <MovieList />
+      <MovieList movies={popularMovies}/>
     </div>
   )
 }
